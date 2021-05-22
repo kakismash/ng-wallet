914822fb0fba202d7402d586a60a1fb5db801cae
Joaquin Navarro <alfian1991@gmail.com>
2021-05-21 21:04:25 -0400

    adding some methods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,33 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'ng-wallet';
 
-  cardNertwork: string[] = ['AMEX', 'VISA', 'MASTERCARD', 'DISCOVER', 'ELECTRON', 'ELO', 'ELO_DEBIT', 'INTERAC', 'JCB', 'MAESTRO'];
+  private acceptedCardTypes: string[] = ['AMEX', 'VISA', 'MASTERCARD', 'DISCOVER', 'ELECTRON', 'ELO', 'ELO_DEBIT', 'INTERAC', 'JCB', 'MAESTRO'];
+  cardTypes: string[] = [];
+
+  constructor() {
+    this.defaultCardTypes();
+  }
+
+  defaultCardTypes(): void {
+    this.cardTypes = [];
+    this.cardTypes.push('VISA');
+    this.cardTypes.push('MASTERCARD');
+  }
+
+  assignCardTypes(values: string[]) {
+    values.forEach(v => {
+      if (this.isAcceptedCardType(v) && !this.isAlreadyCardType(v)) {
+        this.cardTypes.push(v);
+      }
+    });
+  }
+
+  private isAcceptedCardType(value: string): boolean {
+    return this.acceptedCardTypes.includes(value);
+  }
+
+  private isAlreadyCardType(value: string): boolean {
+    return this.cardTypes.includes(value);
+  }
+
 }
