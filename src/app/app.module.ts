08daddbd03727caeec2de38ad05026c97dacdd37
Jose Vicente <jantvb@gmail.com>
2021-05-21 20:09:21 -0400

    Google Pay

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { GooglePayButtonModule } from '@google-pay/button-angular';
 
 import { AppComponent } from './app.component';
+import { GooglePayComponent } from './google-pay/google-pay.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GooglePayComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    GooglePayButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/google-pay/google-pay.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-google-pay',
+  templateUrl: './google-pay.component.html',
+  styleUrls: ['./google-pay.component.scss']
+})
+export class GooglePayComponent implements OnInit {
+
+  paymentRequest: google.payments.api.PaymentDataRequest = {
+    apiVersion: 2,
+    apiVersionMinor: 0,
+    allowedPaymentMethods: [
+      {
+        type: 'CARD',
+        parameters: {
+          allowedAuthMethods: ['PAN_ONLY', 'CRYPTOGRAM_3DS'],
+          allowedCardNetworks: ['AMEX', 'VISA', 'MASTERCARD']
+        },
+        tokenizationSpecification: {
+          type: 'PAYMENT_GATEWAY',
+          parameters: {
+            gateway: 'example',
+            gatewayMerchantId: 'exampleGatewayMerchantId'
+          }
+        }
+      }
+    ],
+    merchantInfo: {
+      merchantId: '12345678901234567890',
+      merchantName: 'Demo Merchant'
+    },
+    transactionInfo: {
+      totalPriceStatus: 'FINAL',
+      totalPriceLabel: 'Total',
+      totalPrice: '0.10',
+      currencyCode: 'USD',
+      countryCode: 'US'
+    },
+    callbackIntents: ['PAYMENT_AUTHORIZATION']
+  };
+
+  onLoadPaymentData = (
+    event: Event
+  ): void => {
+    const eventDetail = event as CustomEvent<google.payments.api.PaymentData>;
+    console.log('load payment data', eventDetail.detail);
+  }
+
+  onPaymenDataAuthorized: google.payments.api.PaymentAuthorizedHandler = (
+    paymentData
+  ) => {
+    console.log('payment authorized', paymentData);
+    return {
+      transactionState: 'SUCCESS'
+    };
+  }
+
+  onError = (event: ErrorEvent): void => {
+    console.error('error', event.error);
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
