5d0bc92546684ca2ea9e9b57476b8fc397db809a
Jose Vicente <jantvb@gmail.com>
2021-05-31 21:59:20 -0400

    Google Pay

--- projects/ng-wallet/src/lib/ng-wallet.component.ts
+++ projects/ng-wallet/src/lib/ng-wallet.component.ts
@@ -9,19 +9,19 @@ export class NgWalletComponent {
   title = 'ng-wallet';
 
   //**********Button Google Configuration********//
-  @Input() paymentRequest!: google.payments.api.PaymentDataRequest;
-  @Input() environment!: google.payments.api.Environment;
-  @Input() existingPaymentMethodRequired!: boolean;
-  @Input() buttonColor?: google.payments.api.ButtonColor;
-  @Input() buttonType?: google.payments.api.ButtonType;
-  @Input() buttonSizeMode?: google.payments.api.ButtonSizeMode;
-  @Input() buttonLocale?: string;
-  @Input() paymentDataChangedCallback?: google.payments.api.PaymentDataChangedHandler;
-  @Input() paymentAuthorizedCallback?: google.payments.api.PaymentAuthorizedHandler;
-  @Input() readyToPayChangeCallback?: (result: any) => void;
-  @Input() loadPaymentDataCallback?: (paymentData: google.payments.api.PaymentData) => void;
-  @Input() cancelCallback?: (reason: google.payments.api.PaymentsError) => void;
-  @Input() errorCallback?: (error: Error) => void;
+  @Input() paymentRequest!:                   google.payments.api.PaymentDataRequest;
+  @Input() environment!:                      google.payments.api.Environment;
+  @Input() existingPaymentMethodRequired!:    boolean;
+  @Input() buttonColor?:                      google.payments.api.ButtonColor;
+  @Input() buttonType?:                       google.payments.api.ButtonType;
+  @Input() buttonSizeMode?:                   google.payments.api.ButtonSizeMode;
+  @Input() buttonLocale?:                     string;
+  @Input() paymentDataChangedCallback?:       google.payments.api.PaymentDataChangedHandler;
+  @Input() paymentAuthorizedCallback?:        google.payments.api.PaymentAuthorizedHandler;
+  @Input() readyToPayChangeCallback?:         (result: any) => void;
+  @Input() loadPaymentDataCallback?:          (paymentData: google.payments.api.PaymentData) => void;
+  @Input() cancelCallback?:                   (reason: google.payments.api.PaymentsError) => void;
+  @Input() errorCallback?:                    (error: Error) => void;
 
   constructor() {}
 
