3f52f16379734d4c0c5835828a4e3f437bc0d292
Jose Vicente <jantvb@gmail.com>
2021-06-05 16:55:01 -0400

    Apple Pay

--- projects/ng-wallet/src/lib/apple-pay/apple-pay.component.ts
+++ projects/ng-wallet/src/lib/apple-pay/apple-pay.component.ts
@@ -8,15 +8,86 @@ import { Component, Input } from '@angular/core';
 export class ApplePayComponent {
 
   // Button configuration
-  @Input() buttonStyle?:       string;
-  @Input() buttonType?:        string;
-  @Input() buttonLocale?:      string;
-  @Input() width?:             string;
-  @Input() height?:            string;
-  @Input() borderRadius?:      string;
+  @Input() buttonStyle?:              string;
+  @Input() buttonType?:               string;
+  @Input() buttonLocale?:             string;
+  @Input() width?:                    string;
+  @Input() height?:                   string;
+  @Input() borderRadius?:             string;
 
   // Define ApplePayPaymentRequest
-  @Input() request!: ApplePayJS.ApplePayPaymentRequest;
+  @Input() paymentRequest:            ApplePayJS.ApplePayPaymentRequest = {
+      countryCode: "US",
+      currencyCode: "USD",
+      supportedNetworks: [
+          "masterCard",
+          "visa"
+      ],
+      merchantCapabilities: [
+          "supports3DS"
+      ],
+      total: {
+          label: "My Store",
+          amount: "9.99"
+      }
+  };
+
+  // Define payment method.
+  @Input() total:                   ApplePayJS.ApplePayLineItem = {
+      label: "Subtotal",
+      type: "final",
+      amount: "35.00"
+  };
+
+  @Input() lineItems:               Array<ApplePayJS.ApplePayLineItem> = [
+      {
+          label: "Subtotal",
+          type: "final",
+          amount: "35.00"
+      },
+      {
+          label: "Free Shipping",
+          amount: "0.00",
+          type: "pending"
+      },
+      {
+          label: "Estimated Tax",
+          amount: "3.06"
+      }
+  ];
+
+  // Define shipping method.
+  @Input() shippingMethods:        Array<ApplePayJS.ApplePayShippingMethod> = [
+      {
+          label: "Free Shipping",
+          detail: "Arrives in 5 to 7 days",
+          amount: "0.00",
+          identifier: "FreeShipping"
+      },
+      {
+          label: "2-hour Shipping",
+          amount: "5.00"
+      }
+  ];
+
+
+  // Define shipping contact.
+  @Input() shippingContact:        ApplePayJS.ApplePayPaymentContact = {
+      emailAddress: "ravipatel@example.com",
+      familyName: "Patel",
+      givenName: "Ravi",
+      phoneNumber: "(408) 555-5555",
+      addressLines: [
+          "1 Infinite Loop"
+      ],
+      locality: "Cupertino",
+      administrativeArea: "CA",
+      postalCode: "95014",
+      country: "United States",
+      countryCode: "US"
+  };
+
+
 
   constructor() { }
 
@@ -28,7 +99,7 @@ export class ApplePayComponent {
 
 
     // Create ApplePaySession
-    const session = new ApplePaySession(3, this.request);
+    const session = new ApplePaySession(3, this.paymentRequest);
 
     session.onvalidatemerchant = event => {
         // Call your own server to request a new merchant session.
@@ -43,23 +114,20 @@ export class ApplePayComponent {
     };
 
     session.onpaymentmethodselected = event => {
-        // Define ApplePayPaymentMethodUpdate based on the selected payment method.
-        // No updates or errors are needed, pass an empty object.
-        const update = {};
-        session.completePaymentMethodSelection(update);
+        session.completePaymentMethodSelection(this.total, this.lineItems);
     };
 
     session.onshippingmethodselected = event => {
-        // Define ApplePayShippingMethodUpdate based on the selected shipping method.
-        // No updates or errors are needed, pass an empty object.
-        const update = {};
-        session.completeShippingMethodSelection(update);
+        session.completeShippingMethodSelection(ApplePaySession.STATUS_SUCCESS,
+                                                this.total,
+                                                this.lineItems);
     };
 
     session.onshippingcontactselected = event => {
-        // Define ApplePayShippingContactUpdate based on the selected shipping contact.
-        const update = {};
-        session.completeShippingContactSelection(update);
+        session.completeShippingContactSelection(ApplePaySession.STATUS_INVALID_SHIPPING_POSTAL_ADDRESS,
+                                                  this.shippingMethods,
+                                                  this.total,
+                                                  this.lineItems);
     };
 
     session.onpaymentauthorized = event => {
